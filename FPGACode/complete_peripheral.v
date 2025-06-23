4374d4b457902c4e2aba8c4987b0236280733446
André Luiz Vasconcelos Ferreira <andreluizvf123@gmail.com>
2025-06-23 18:57:17 -0300

    Change dimensions of benchmark lattice and add the calculation of the hamiltonian and the avage spin direction

--- FPGACode/complete_peripheral.v
+++ FPGACode/complete_peripheral.v
@@ -3,13 +3,13 @@ module teste();
 reg clk, reset;
 reg enable_white, enable_grey;
 
-reg lattice [7:0][7:0];
-wire final_lattice [7:0][7:0];
+reg lattice [31:0][31:0];
+wire final_lattice [31:0][31:0];
 
 integer counter;
 wire [31:0] rand32;
 integer k,l;
-reg [7:0] mem[7:0];
+reg [31:0] mem[31:0];
 
 Sfrl_32 rand32_mod(
 	.clk(clk),
@@ -21,29 +21,27 @@ initial begin
 	enable_white = 1;
 	enable_grey = 0;
 	counter=0;
-	$display("Ising model sim");
-	$readmemb("memory.mem", mem, 0,7);
+	$readmemb("memory.mem", mem, 0,31);
 	
-	for (k = 0; k < 8; k = k + 1)begin
-	for (l = 0; l < 8; l = l + 1) begin
+	for (k = 0; k < 32; k = k + 1)begin
+	for (l = 0; l < 32; l = l + 1) begin
 		lattice[k][l] = mem[k][l];
 	end
-	$write("\n");
 	end
 end
 
 genvar i,j;
 generate
-    for (i = 0; i < 8; i = i + 1) begin
-    for (j = 0; j < 8; j = j + 1) begin : spins_white  
+    for (i = 0; i < 32; i = i + 1) begin
+    for (j = 0; j < 32; j = j + 1) begin : spins_white  
     	if((i%2==0 & j%2==0) | (i%2!=0 & j%2!=0)) begin
 		   Spin spin_inst(
 		   	.clk(clk),
 		   	.spin_val(lattice[i][j]), 
-		   	.left(lattice[(i+7)%8][j]), // (i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
-		   	.right(lattice[(i+1)%8][j]), 
-		   	.top(lattice[i][(j+7)%8]),// (i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2 
-		   	.bottom(lattice[i][(j+1)%8]), 
+		   	.left(lattice[(i+31)%32][j]), // (i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
+		   	.right(lattice[(i+1)%32][j]), 
+		   	.top(lattice[i][(j+31)%32]),// (i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2 
+		   	.bottom(lattice[i][(j+1)%32]), 
 		   	.rand32(rand32), 
 		   	.enable(enable_white),
 		   	.final_spin_val(final_lattice[i][j])
@@ -51,16 +49,16 @@ generate
 	   end
     end
     end
-    for (i = 0; i < 8; i = i + 1) begin
-    for (j = 0; j < 8; j = j + 1) begin : spins_grey   
+    for (i = 0; i < 32; i = i + 1) begin
+    for (j = 0; j < 32; j = j + 1) begin : spins_grey   
     	if((i%2==0 & j%2!=0) | (i%2!=0 & j%2==0)) begin
 		   Spin spin_inst(
    		   	.clk(clk),
 		   	.spin_val(lattice[i][j]), 
-		   	.left(lattice[(i+7)%8][j]), //(i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
-		   	.right(lattice[(i+1)%8][j]), 
-		   	.top(lattice[i][(j+7)%8]), //(i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
-		   	.bottom(lattice[i][(j+1)%8]), 
+		   	.left(lattice[(i+31)%32][j]), //(i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
+		   	.right(lattice[(i+1)%32][j]), 
+		   	.top(lattice[i][(j+31)%32]), //(i+-1+8)%8 --> se i=0, então 7%8 = 7. Se i>0. então i=3 --> 3-1+8=10 ---> 10%8 = 2
+		   	.bottom(lattice[i][(j+1)%32]), 
 		   	.rand32(rand32), 
 		   	.enable(enable_grey),
 		   	.final_spin_val(final_lattice[i][j])
@@ -72,16 +70,20 @@ endgenerate
 
 always #1 clk = (clk===1'b0);
 
+integer energy, spin_dir;
+
 always @(posedge clk or posedge reset) begin
-    if(counter<20) begin
-    	    if(counter%2==0 && counter > 0) begin
-		    for (k = 0; k < 8; k = k + 1) begin
-		    for (l = 0; l < 8; l = l + 1) begin  
+    if(counter<1000) begin
+    	    //Update lattice
+    	    if(counter > 1) begin
+		    for (k = 0; k < 32; k = k + 1) begin
+		    for (l = 0; l < 32; l = l + 1) begin  
 			lattice[k][l] = final_lattice[k][l];
 		    end
 		    end 	    
     	    end
     	    
+    	    //Alternate between white and grey set of lattices. This mechanism allows paralelism
 	    if (reset) begin
 		enable_white = 1;
 		enable_grey = 0;
@@ -90,16 +92,39 @@ always @(posedge clk or posedge reset) begin
 		enable_grey  = ~enable_grey;
 	    end 
 	    counter++;
-	    
-    	    	
+	    if(counter%4 == 0) begin
+	    energy = 0;
+    	    spin_dir = 0;
+	    //Calculate hamiltonian
+	    for (k = 0; k < 32; k = k + 1) begin
+	    for (l = 0; l < 32; l = l + 1) begin  
+	        
+		energy += (-2*lattice[k][l]-1)*(
+			(2*lattice[(k+31)%32][l]-1)+
+		   	(2*lattice[(k+1)%32][l]-1)+ 
+		   	(2*lattice[k][(l+31)%32]-1)+
+		   	(2*lattice[k][(l+1)%32]-1));
+	    end
+	    end
+    	    
+    	    //Calculate spin avg dir
+    	    for (k = 0; k < 32; k = k + 1) begin
+	    for (l = 0; l < 32; l = l + 1) begin  
+		spin_dir += 2*lattice[k][l]-1;
+	    end
+	    end
+	    end
+	    //Write in file
+	    $write("%d,%.3f,%.3f\n",counter,energy,spin_dir*0.0009765625);
     		
     /*for (k = 0; k < 8; k = k + 1) begin
     for (l = 0; l < 8; l = l + 1) begin  
 	$write("%d ",final_lattice[k][l]);
     end
         $write("\n");
-    end*/
-    $write("\n");
+    end
+    $write("\n");*/
+    
     end else begin
     	$finish;
     end
