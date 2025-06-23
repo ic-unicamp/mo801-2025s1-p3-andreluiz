59131877b5c84e10d6a2f8f0af4f16de4b11e11b
André Luiz Vasconcelos Ferreira <andreluizvf123@gmail.com>
2025-06-22 21:32:42 -0300

    Implement a lattice generator in C

---
+++ FPGACode/lattice_gen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define N 8
+#define Temperatura 5
+//#define DEBUG
+
+void create_spin_lattice(int proportion){
+  FILE *memory;
+  memory = fopen("memory.mem","w");
+  
+  if (memory == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return;
+  }
+  
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      float random = (rand() % 1000) + 1;
+      char bit;
+      if(random >= proportion)
+        bit = 1;
+      else
+        bit = 0;
+      
+      fprintf(memory,"%c",bit+'0');
+    }
+      fprintf(memory,"\n");
+  }
+  fclose(memory);
+}
+
+int main() {
+    srand(time(NULL));
+    
+    //Prepara o modelo
+    create_spin_lattice(750);
+
+    return 0;
+}
